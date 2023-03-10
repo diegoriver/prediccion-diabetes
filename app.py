90862e6017141b9330d83ff8804a41ff0d38d2a2
diegoriver <telmework@gmail.com>
2023-03-09 22:14:02 -0500

    arreglo enunciados y organizacion resultado-dataframe

--- app.py
+++ app.py
@@ -37,29 +37,29 @@ def prediction():
     # Lecctura de datos
     #Datos = st.text_input("Ingrese los valores para predecir diabetes:")
     
-    HighBP_formulario = st.radio( "1. Presion Alta:",('Sin presion Alta', 'con presion Alta'))
+    HighBP_formulario = st.radio("1.Presión Alta:",('Sin presion Alta', 'Con presión Alta'))
     if HighBP_formulario == 'Sin presion Alta': HighBP = 0
     else: HighBP = 1
     
-    HighChol_formulario = st.radio( "2. Colesterol: ",('Sin colesterol alto', 'Con colesterol Alto'))
+    HighChol_formulario = st.radio( "2.Colesterol: ",('Sin colesterol alto', 'Con colesterol Alto'))
     if HighChol_formulario == 'Sin colesterol alto': HighChol = 0
     else: HighChol = 1
 
-    CholCheck_formulario = st.radio( "3. Chequeo de Colesterol: ",('Ningún control de colesterol en 5 años', 'Control de colesterol en 5 años'))
+    CholCheck_formulario = st.radio( "3.Chequeo de Colesterol: ",('Ningún control de colesterol en 5 años', 'Control de colesterol en 5 años'))
     if CholCheck_formulario == 'Ningún control de colesterol en 5 años': CholCheck = 0
     else: CholCheck = 1
 
-    BMI = st.slider('4. Indice de Masa Corporal:', 10, 45, 27)
+    BMI = st.slider('4.Indice de Masa Corporal:', 10, 45, 27)
 
     Smoker_formulario = st.radio( "5.Fumador: ¿Ha fumado al menos 100 cigarrillos en toda su vida? [Nota: 5 paquetes = 100 cigarrillos] ",('No', 'Si'))
     if Smoker_formulario == 'No': Smoker = 0
     else: Smoker = 1
 
-    Stroke_formulario = st.radio( "6. Derrame Cerebral: (Alguna vez te dijeron que tuviste un derrame cerebral.)",('No', 'Si'))
+    Stroke_formulario = st.radio( "6.Derrame Cerebral: (Alguna vez te dijeron que tuviste un derrame cerebral.)",('No', 'Si'))
     if Stroke_formulario == 'No': Stroke = 0
     else: Stroke = 1
 
-    HeartDiseaseorAttack_formulario = st.radio( "7. Enfermedad del Corazon: (Enfermedad Coronaria (CHD) o infarto de miocardio (IM))",('No', 'Si'))
+    HeartDiseaseorAttack_formulario = st.radio( "7.Enfermedad del Corazon: (Enfermedad Coronaria (CHD) o infarto de miocardio (IM))",('No', 'Si'))
     if HeartDiseaseorAttack_formulario == 'No': HeartDiseaseorAttack = 0
     else: HeartDiseaseorAttack = 1
 
@@ -67,27 +67,27 @@ def prediction():
     if PhysActivity_formulario == 'No': PhysActivity = 0
     else: PhysActivity = 1
 
-    Fruits_formulario = st.radio( "9. Consume Frutas: (Consume frutaa 1 o más veces al día?)",('No', 'Si'))
+    Fruits_formulario = st.radio( "9.Consume Frutas: (Consume frutaa 1 o más veces al día?)",('No', 'Si'))
     if Fruits_formulario == 'No': Fruits = 0
     else: Fruits = 1
 
-    Veggies_formulario = st.radio( "10. Consume Verduras: (Consume Verduras 1 o más veces al día?)",('No', 'Si'))
+    Veggies_formulario = st.radio( "10.Consume Verduras: (Consume Verduras 1 o más veces al día?)",('No', 'Si'))
     if Veggies_formulario == 'No': Veggies = 0
     else: Veggies = 1
 
-    HvyAlcoholConsump_formulario = st.radio( "11. Bebedor Empedernido: (Hombres adultos que toman más de 14 bebidas por semana y Mujeres adultas que toman más de 7 bebidas por semana.)",('No', 'Si'))
+    HvyAlcoholConsump_formulario = st.radio( "11.Bebedor Empedernido: (Hombres adultos que toman más de 14 bebidas por semana y Mujeres adultas que toman más de 7 bebidas por semana.)",('No', 'Si'))
     if HvyAlcoholConsump_formulario == 'No': HvyAlcoholConsump = 0
     else: HvyAlcoholConsump = 1
 
-    AnyHealthcare_formulario = st.radio( "12. Cobertura en Salud: (Tiene cualquier tipo de cobertura de atención médica, incluido seguro de salud, planes prepagados como HMO, etc.)",('No', 'Si'))
+    AnyHealthcare_formulario = st.radio( "12.Cobertura en Salud: (Tiene cualquier tipo de cobertura de atención médica, incluido seguro de salud, planes prepagados como HMO, etc.)",('No', 'Si'))
     if AnyHealthcare_formulario == 'No': AnyHealthcare = 0
     else: AnyHealthcare = 1
 
-    NoDocbcCost_formulario = st.radio( "13. Consulta Medica: (¿Hubo un momento en los últimos 12 meses en el que necesitó ver a un médico pero no pudo debido al costo?)",('No', 'Si'))
+    NoDocbcCost_formulario = st.radio( "13.Consulta Medica: (¿Hubo un momento en los últimos 12 meses en el que necesitó ver a un médico pero no pudo debido al costo?)",('No', 'Si'))
     if NoDocbcCost_formulario == 'No': NoDocbcCost = 0
     else: NoDocbcCost = 1
 
-    GenHlth_formulario = st.radio( "14. Estado de Salud : (Consideras tu estado de salud)",
+    GenHlth_formulario = st.radio( "14.Estado de Salud : (Consideras tu estado de salud)",
     ('Excelente', 'Muy Bueno', 'Bueno', 'Justo', 'Pobre'))
     if GenHlth_formulario == 'Excelente': GenHlth = 1
     if GenHlth_formulario == 'Muy Bueno': GenHlth = 2
@@ -95,19 +95,19 @@ def prediction():
     if GenHlth_formulario == 'Justo': GenHlth = 4
     if GenHlth_formulario == 'Pobre': GenHlth = 5
     
-    MentHlth = st.slider('15. Salud Mental : (Durante cuantos dias en el ultimo mes ha sufrido de: Stres,Depresion o Emocion. Escala de 0 a 30)', 0, 30, 12)
+    MentHlth = st.slider('15.Salud Mental : (Durante cuantos dias en el ultimo mes ha sufrido de: Stres,Depresion o Emocion. Escala de 0 a 30)', 0, 30, 12)
     
-    PhysHlth = st.slider('16. Salud Fisica: (Durante cuantos dias en el ultimo mes su salud fisica no es buena)', 0, 30, 15)
+    PhysHlth = st.slider('16.Salud Fisica: (Durante cuantos dias en el ultimo mes su salud fisica no es buena)', 0, 30, 15)
     
-    DiffWalk_formulario = st.radio( "17. Dificultad Para Caminar: ¿Tiene serias dificultades para caminar o subir escaleras?",('No', 'Si'))
+    DiffWalk_formulario = st.radio( "17.Dificultad Para Caminar: ¿Tiene serias dificultades para caminar o subir escaleras?",('No', 'Si'))
     if DiffWalk_formulario == 'No': DiffWalk = 0
     else: DiffWalk = 1
 
-    Sex_formulario = st.radio( "18. Sexo:",('Femenino', 'Masculino'))
+    Sex_formulario = st.radio( "18.Sexo:",('Femenino', 'Masculino'))
     if Sex_formulario == 'Femenino': Sex = 0
     else: Sex = 1
     
-    Age_formulario = st.radio( "19. Edad:",('De 18 a 24 años', 'De 25 a 29', 'De 30 a 34', 'De 35 a 39', 'De 40 a 44', 'De 45 a 49', 
+    Age_formulario = st.radio( "19.Edad:",('De 18 a 24 años', 'De 25 a 29', 'De 30 a 34', 'De 35 a 39', 'De 40 a 44', 'De 45 a 49', 
     'De 50 a 54','De 55 a 59', 'De 60 a 64', 'De 65 a 69', 'De 70 a 74', 'De 75 a 79', '80 a mas'))
     if Age_formulario == 'De 18 a 24 años': Age = 1
     if Age_formulario == 'De 25 a 29': Age = 2
@@ -123,7 +123,7 @@ def prediction():
     if Age_formulario == 'De 75 a 79': Age = 12
     if Age_formulario == '80 a mas': Age = 13
 
-    Education_formulario = st.radio( "20. Nivel de Educacion:",
+    Education_formulario = st.radio( "20.Nivel de Educacion:",
     ('Nunca asistió a la escuela o solo al jardín de infantes', 
     'Grados 1 a 8 (Primaria)', 
     'Grados 9 a 11 (Alguna escuela secundaria)', 
@@ -138,7 +138,7 @@ def prediction():
     if Education_formulario == 'Universidad 1 año a 3 años (Alguna universidad o escuela técnica)': Education = 5
     if Education_formulario == 'Universidad 4 años o más (graduado universitario)': Education = 6
 
-    Income_formulario = st.radio( "21. Ingresos: ",
+    Income_formulario = st.radio( "21.Ingresos: ",
     ('Menos de 10.000', 
     'De 10.000 a 16.000', 
     'De 17.000 a 22.000', 
@@ -158,8 +158,8 @@ def prediction():
     if Income_formulario == 'De 62.000 a 75.000': Income = 8
 
     ## se crea un dataframe vacio
-    df = pd.DataFrame(columns=['% NO DIABÉTICO', '% PRE DIABÉTICO', '% DIABÉTICO'])
-    placeholder = st.empty()
+    # df = pd.DataFrame(columns=['% NO DIABÉTICO', '% PRE DIABÉTICO', '% DIABÉTICO'])
+    # placeholder = st.empty()
 
     # El botón predicción se usa para iniciar el procesamiento
     if st.button("Predicción:"):
@@ -198,13 +198,25 @@ def prediction():
         elif i == 2:
             resultado_final = "Diabetico"
 
-        df.loc[i+1] = ["{:.2f}".format(n[0]), "{:.2f}".format(n[1]), "{:.2f}".format(n[2])]
-        with placeholder.container():
-            st.dataframe(df)
-        st.success(f"{resultado_final}")
+        # df.loc[i+1] = ["{:.2f}".format(n[0]), "{:.2f}".format(n[1]), "{:.2f}".format(n[2])]
+        # with placeholder.container():
+        #     st.dataframe(df)
+        # st.success(f"{resultado_final}")
 
-        return (n)
+        return (n, resultado_final)
 
 if __name__ == '__main__':
-    prediction_matrix = prediction()
+    a = 1
+    df = pd.DataFrame(columns=['% NO DIABÉTICO', '% PRE DIABÉTICO', '% DIABÉTICO'])
+    placeholder = st.container()
+
+    n, resultado_final = prediction()
+    
+    st.success(f"{resultado_final}")
+
+    df.loc[a+1] = ["{:.2f}".format(n[0]),"{:.2f}".format(n[1]), "{:.2f}".format(n[2])]
+    
+    with placeholder.container():
+        st.dataframe(df)
+    
     
